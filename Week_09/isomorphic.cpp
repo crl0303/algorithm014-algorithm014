bbb2922621c2d2320a5d84b79cdeecf7fcd534e6
crl0303 <rtmx_cuirl@163.com>
2020-11-03 00:25:37 +0800

    add class Solution {

---
+++ Week_09/isomorphic.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size())
+        {
+            return false;
+        }
+        if(s.size() == 0 && t.size() == 0)
+            return true;
+        for (int i=0; i < s.size(); i++)
+        {
+            if(s.find(s[i]) != t.find(t[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+int main ()
+{
+    string s;
+    cin>>s;
+    string t;
+    cin >> t;
+    bool ret = false;
+    Solution so; 
+    ret = so.isIsomorphic(s, t);
+    //if(s.compare("leetcode") == 0 && ret == 0)
+    {
+        cout << "s="<<s<< " t="<< t<<" ret="<<ret<<endl;
+    }
+    return 0;
+}
